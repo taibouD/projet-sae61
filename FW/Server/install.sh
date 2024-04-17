70291b865d28d65c4fd7d28320c31c19e0f31de1
root <root@debian>
2024-04-17 15:22:05 +0200

    derniers mmaj

--- FW/Server/install.sh
+++ FW/Server/install.sh
@@ -4,7 +4,11 @@
 sudo apt-get update
 
 # Installation de Nginx
-sudo apt-get install -y nginx
+sudo apt-get install -y nginx ufw
+sudo ufw allow 'Nginx HTTP'
+sudo ufw enable
+sudo systemctl start nginx
+
 
 # Copie du fichier index.html
 sudo cp /home/user/projet-sae61/FW/Server/index.html /var/www/html/
